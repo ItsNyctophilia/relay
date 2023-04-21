a04334342ec04c7cb2acc715bb7da70d521e0700
James Viner Esq <jrviner2000@gmail.com>
2023-04-21 15:35:25 -0400

    Correctly close listeners on EOF

--- src/dispatcher/socket_client.c
+++ src/dispatcher/socket_client.c
@@ -25,64 +25,62 @@ bool message_ready = false;
 char buffer[BUFSIZ];
 mtx_t lock;
 
-
-
-static void cleanup_threads(struct thread_pool *trds, thrd_t input, thrd_t conn) {
-        for (size_t n = 0; n < trds->thrd_sz; ++n) {
-                if (trds->threads[n]) {
-                        thrd_join(trds->threads[n], NULL);
-                }
-        }
-        if (trds->threads) {
-                free(trds->threads);
-        }
-        if (trds->thread_fds) {
-                free(trds->thread_fds);
-        }
-        if (input){
-                thrd_join(input, NULL);
-        }
-        if (conn){
-                thrd_join(conn, NULL);
-        }
-        for (size_t n = 0; n < trds->thrd_sz; ++n) {
-                if (trds->client_objs[n]) {
-                        if (trds->client_objs[n] != NULL){
-                                free(trds->client_objs[n]);
-                        }
-                }
-        }
-        free(trds->client_objs);
+static void cleanup_threads(struct thread_pool *trds, thrd_t input, thrd_t conn)
+{
+	for (size_t n = 0; n < trds->thrd_sz; ++n) {
+		if (trds->threads[n]) {
+			thrd_join(trds->threads[n], NULL);
+		}
+	}
+	if (trds->threads) {
+		free(trds->threads);
+	}
+	if (trds->thread_fds) {
+		free(trds->thread_fds);
+	}
+	if (input) {
+		thrd_join(input, NULL);
+	}
+	if (conn) {
+		thrd_join(conn, NULL);
+	}
+	for (size_t n = 0; n < trds->thrd_sz; ++n) {
+		if (trds->client_objs[n]) {
+			if (trds->client_objs[n] != NULL) {
+				free(trds->client_objs[n]);
+			}
+		}
+	}
+	free(trds->client_objs);
 }
 
 static struct client_data *create_client(int sd, int *rtn)
 {
-        struct pollfd input = { .fd = sd, .events = POLLIN };
-        int result = poll(&input, 1,  500);
-        if (result == 0) {
-                *rtn = NTS;
-                return NULL;
-        }
+	struct pollfd input = {.fd = sd,.events = POLLIN };
+	int result = poll(&input, 1, 500);
+	if (result == 0) {
+		*rtn = NTS;
+		return NULL;
+	}
 	struct client_data *client = calloc(1, sizeof(*client));
 	if (client == NULL) {
 		perror("Failed to allocate memory for client");
 		*rtn = MEMORY_ALLOCATION_ERROR;
-        return NULL;
+		return NULL;
 	}
 	client->client_sz = sizeof(client->client_strg);
 
+	client->client_socket =
+	    accept(sd, (struct sockaddr *)&client->client_strg,
+		   &client->client_sz);
 
-    client->client_socket =
-                    accept(sd, (struct sockaddr *)&client->client_strg,
-                           &client->client_sz);
-
-    if (client->client_socket == -1) {
-        perror("Failed to accept client");
-        free(client);
-        return NULL;
-    }
-    client->ptr.sd = client->client_socket;
-    *rtn = CLIENT_SUCCESS;
+	if (client->client_socket == -1) {
+		perror("Failed to accept client");
+		free(client);
+		return NULL;
+	}
+	client->ptr.sd = client->client_socket;
+	*rtn = CLIENT_SUCCESS;
 	return client;
 }
 
@@ -119,8 +117,8 @@ int handle_client(void *handle)
 	while (break_loop) {
 
 	}
-    strcpy(buffer, "\07");
-    write(client.sd, buffer, BUFSIZ - 1);
+	strcpy(buffer, "\07");
+	write(client.sd, buffer, BUFSIZ - 1);
 
 	close(client.sd);
 	return 0;
@@ -128,124 +126,138 @@ int handle_client(void *handle)
 
 static int input_thread_func(void *arg)
 {
-        union thread_ptr trd = {.ptr = arg };
-        struct thread_pool *trds = trd.tp;
-        struct pollfd input = { .fd = 0, .events = POLLIN };
-        while (break_loop) {
-                int result = poll(&input, 1,  500);
-                if (input.revents & POLLIN) {
-                        fgets(buffer, BUFSIZ - 1, stdin);
-                        mtx_lock(&lock);
-                        for (size_t n = 0; n < trds->thrd_sz; ++n) {
-                                if (trds->thread_fds[n] == -1) {
-                                        continue;
-                                }
-                                ssize_t amount = write(trds->thread_fds[n], buffer, strlen(buffer));
-                                if (amount < 0 && errno != EAGAIN) {
-                                        perror("Unable to read from client");
-                                        close(trds->thread_fds[n]);
-                                        return CLIENT_WRITE_ERROR;
-                                }
-                        }
-                        mtx_unlock(&lock);
-                }
-        }
-        return SUCCESS;
+	union thread_ptr trd = {.ptr = arg };
+	struct thread_pool *trds = trd.tp;
+	struct pollfd input = {.fd = 0,.events = POLLIN };
+	while (break_loop) {
+		int result = poll(&input, 1, 500);
+		if (result < 0) {
+			perror("Error received while polling");
+			return FAILURE;
+		}
+		if (input.revents & POLLIN) {
+			char *err = fgets(buffer, BUFSIZ - 1, stdin);
+			if (!err) {
+				break_loop = false;
+				break;
+			}
+			mtx_lock(&lock);
+			for (size_t n = 0; n < trds->thrd_sz; ++n) {
+				if (trds->thread_fds[n] == -1) {
+					continue;
+				}
+				ssize_t amount =
+				    write(trds->thread_fds[n], buffer,
+					  strlen(buffer));
+				if (amount < 0 && errno != EAGAIN) {
+					perror("Unable to read from client");
+					close(trds->thread_fds[n]);
+					return CLIENT_WRITE_ERROR;
+				}
+			}
+			mtx_unlock(&lock);
+		}
+	}
+
+	return SUCCESS;
 }
 
 static int connection_thread_func(void *arg)
 {
-        union thread_ptr trd = {.ptr = arg };
-        struct thread_pool *trds = trd.tp;
-        trds->client_objs = malloc(trds->thrd_cap * sizeof(*trds->client_objs));
-        while (break_loop) {
-                int rtn;
-                struct client_data *c_data = create_client(trd.tp->sd, &rtn);
-                if (rtn == MEMORY_ALLOCATION_ERROR) {
-                        perror("Unable to create client!!");
-                        return CLIENT_CREATE_FAIL;
-                } else if (rtn == NTS){
-                        continue;
-                } else if (rtn == CLIENT_SUCCESS) {
-                        // Add client to client array
-                        mtx_lock(&lock);
-                        trds->client_objs[trds->thrd_sz] = c_data;
-                        mtx_unlock(&lock);
-                        }
+	union thread_ptr trd = {.ptr = arg };
+	struct thread_pool *trds = trd.tp;
+	trds->client_objs = malloc(trds->thrd_cap * sizeof(*trds->client_objs));
+	while (break_loop) {
+		int rtn;
+		struct client_data *c_data = create_client(trd.tp->sd, &rtn);
+		if (rtn == MEMORY_ALLOCATION_ERROR) {
+			perror("Unable to create client!!");
+			return CLIENT_CREATE_FAIL;
+		} else if (rtn == NTS) {
+			continue;
+		} else if (rtn == CLIENT_SUCCESS) {
+			// Add client to client array
+			mtx_lock(&lock);
+			trds->client_objs[trds->thrd_sz] = c_data;
+			mtx_unlock(&lock);
+		}
 
-                mtx_lock(&lock);
-                trds->thread_fds[trds->thrd_sz] = c_data->client_socket;
-                int err =
-                        thrd_create(&trds->threads[trds->thrd_sz++], handle_client,
-                        c_data->ptr.ptr);
+		mtx_lock(&lock);
+		trds->thread_fds[trds->thrd_sz] = c_data->client_socket;
+		int err =
+		    thrd_create(&trds->threads[trds->thrd_sz++], handle_client,
+				c_data->ptr.ptr);
 
+		if (err != SUCCESS) {
+			perror("Unable to create thread");
+			return THREAD_CREATE_ERROR;
+		}
 
-                if (err != SUCCESS) {
-                        perror("Unable to create thread");
-                        return THREAD_CREATE_ERROR;
-                }
-
-                if (trds->thrd_sz == trds->thrd_cap) {
-                        thrd_t *tmp =
-                        realloc(trds->threads,
-                        2 * trds->thrd_cap * sizeof(*tmp));
-                        int *tmp2 =
-                        realloc(trds->thread_fds,
-                            2 * trds->thrd_cap * sizeof(*tmp2));
-                        struct client_data **tmp3 = realloc(trds->client_objs,
-                                       2 * trds->thrd_sz * sizeof(*tmp3));
-                        if (!tmp || !tmp2 || !tmp3) {
-                                perror("Cannot create more threads");
-                                break;
-                        }
-                        trds->threads = tmp;
-                        trds->thread_fds = tmp2;
-                        trds->client_objs = tmp3;
-                        trds->thrd_cap *= 2;
-		        }
-                mtx_unlock(&lock);
-        }
-        return SUCCESS;
+		if (trds->thrd_sz == trds->thrd_cap) {
+			thrd_t *tmp = realloc(trds->threads,
+					      2 * trds->thrd_cap *
+					      sizeof(*tmp));
+			int *tmp2 = realloc(trds->thread_fds,
+					    2 * trds->thrd_cap * sizeof(*tmp2));
+			struct client_data **tmp3 = realloc(trds->client_objs,
+							    2 * trds->thrd_sz *
+							    sizeof(*tmp3));
+			if (!tmp || !tmp2 || !tmp3) {
+				perror("Cannot create more threads");
+				break;
+			}
+			trds->threads = tmp;
+			trds->thread_fds = tmp2;
+			trds->client_objs = tmp3;
+			trds->thrd_cap *= 2;
+		}
+		mtx_unlock(&lock);
+	}
+	return SUCCESS;
 }
 
 int start_client_loop(int sd)
 {
 
-        struct thread_pool trds = { 0 };
-        trds.sd = sd;
-        int rtn = setup_threads(&trds);
-        trds.thread_fds = malloc(trds.thrd_cap * sizeof(*trds.thread_fds));
+	struct thread_pool trds = { 0 };
+	trds.sd = sd;
+	int rtn = setup_threads(&trds);
+	trds.thread_fds = malloc(trds.thrd_cap * sizeof(*trds.thread_fds));
 
-        int err = SUCCESS;
-        if (rtn != SUCCESS) {
-                perror("Unable to setup threads");
-                err = rtn;
-        }
+	int err = SUCCESS;
+	if (rtn != SUCCESS) {
+		perror("Unable to setup threads");
+		err = rtn;
+	}
 
-        thrd_t input_thread;
-        if (err == SUCCESS) {
+	thrd_t input_thread;
+	if (err == SUCCESS) {
 
-                union thread_ptr input_ptr = {.tp = &trds};
-                err = thrd_create(&input_thread, input_thread_func, input_ptr.ptr);
-                if (err != SUCCESS) {
-                        perror("Unable to create thread");
-                        err = THREAD_CREATE_ERROR;
-                }
-        }
+		union thread_ptr input_ptr = {.tp = &trds };
+		err =
+		    thrd_create(&input_thread, input_thread_func,
+				input_ptr.ptr);
+		if (err != SUCCESS) {
+			perror("Unable to create thread");
+			err = THREAD_CREATE_ERROR;
+		}
+	}
 
-        thrd_t connection_thread;
-        if (err == SUCCESS) {
-                union thread_ptr conn_ptr = {.tp = &trds};
-                err = thrd_create(&connection_thread, connection_thread_func, conn_ptr.ptr);
-                if (err != SUCCESS) {
-                        perror("Unable to create thread");
-                        err = THREAD_CREATE_ERROR;
-                }
-        }
-        while (break_loop) {
+	thrd_t connection_thread;
+	if (err == SUCCESS) {
+		union thread_ptr conn_ptr = {.tp = &trds };
+		err =
+		    thrd_create(&connection_thread, connection_thread_func,
+				conn_ptr.ptr);
+		if (err != SUCCESS) {
+			perror("Unable to create thread");
+			err = THREAD_CREATE_ERROR;
+		}
+	}
+	while (break_loop) {
 
-        }
-        cleanup_threads(&trds, input_thread, connection_thread);
+	}
+	cleanup_threads(&trds, input_thread, connection_thread);
 
-	    return err;
+	return err;
 }
